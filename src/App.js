6d7dd39d62c88ec5da31e9db4797ce0a68579476
Cagan Sevencan <cagansvncn@gmail.com>
2020-06-19 21:44:55 -0700

    CSS editing

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import HideableText from "./HideableText";
 import AutoCompleteText from "./AutoComplete";
-import healthWords from "./healthWords";
+import diseaseWords from "./diseaseWords";
 
 function App() {
-  return (
-    <div className="App">
-      <div className={"App-Component"}>
-          <AutoCompleteText items={healthWords} />
-      </div>
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <h2 className={"App-Margin"}>
+                Search for diseases:
+            </h2>
+            <div className={"App-Component"}>
+                <AutoCompleteText items={diseaseWords}/>
+            </div>
+            <br/><br/>
+            <h2 className={"App-Margin"}>
+                Search for drugs:
+            </h2>
+            <div className={"App-Component"}>
+                <AutoCompleteText items={diseaseWords}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
